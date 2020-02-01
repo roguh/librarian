b2f395e31050f3ccbfd2b70e70717bbc5724e39e
Hugo O. Rivera <6373447+roguh@users.noreply.github.com>
2020-01-31 16:41:32 -0800

    Add process_html function

--- processor/scrape_queue_worker.py
+++ processor/scrape_queue_worker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import asyncio
+import datetime
 import json
 import os
 import signal
@@ -17,9 +18,11 @@ from newspaper import Article
 def running_in_docker():
     return Path('/.dockerenv').exists()
 
+
 load_dotenv(dotenv_path=Path('../postgres_dev.env'))
 
 NATS_ADDR = 'nats' if running_in_docker() else 'localhost'
+NATS_PORT = 4333
 PG_USER = os.getenv('POSTGRES_USER')
 PG_PWD = os.getenv('POSTGRES_PASSWORD')
 PG_DB = os.getenv('POSTGRES_DB')
@@ -27,41 +30,53 @@ PG_ADDR = 'postgres' if running_in_docker() else 'localhost'
 PG_CONNECTION = f'postgresql://{PG_USER}:{PG_PWD}@{PG_ADDR}/{PG_DB}'
 
 
+def process_html(url, html):
+    print('Processing', url)
+    article = Article(url)
+    article.download(input_html=html)
+    article.parse()
+    article.authors = '; '.join(article.authors)
+    print('Parsed', len(article.text),
+          'bytes of natural text by', article.authors)
+    # article.nlp()  # keywords, summary
+    return article
+
+
 async def run(loop):
     conn = await asyncpg.connect(PG_CONNECTION)
     print('Connected to Postgres')
 
     nc = NATS()
-    await nc.connect(f'nats://{NATS_ADDR}:4222', loop=loop)
+    await nc.connect(f'nats://{NATS_ADDR}:{NATS_PORT}', loop=loop)
 
     session = aiohttp.ClientSession()
 
     async def urls_handler(msg):
         # Download and place in html column of Content table
         data = json.loads(msg.data.decode())
-        print(f'Received a message subject="{msg.subject}" ({msg.reply}): {data}')
+        print(f'Received a message in {msg.subject} ({msg.reply}): {data}')
         url = data['url']
-        print('Processing', url)
         async with session.get(url) as resp:
             html = await resp.text()
-        print('Downloaded HTML', len(html), 'bytes')
-        await conn.execute('INSERT INTO "Contents" (url, html) VALUES $1, $2', url, html)
-        print('added to db')
-        article = Article(url)
-        article.download(input_html=html)
-        article.parse()
-        article.nlp() # keywords, summary
-        article_attrs = 'title meta_lang meta_description top_image images authors text'.split()
-        print('CONTENT:', article['title'], article['authors'], article['meta_description'], article['text'][:128])
-        print('Inserting content into database')
-        q = f'INSERT INTO "Contents" (url, html, {", ".join(article_attrs)}) ' + \
-            f'VALUES ($1), ($2) {", ".join("(${})".format(i + 3) for i in range(len(article_attrs)))}'
+        print('Downloaded', len(html), 'bytes of HTML')
+        article = process_html(url, html)
+        # TODO images
+        attrs = 'title meta_lang meta_description top_image authors text'.split()
+        q = 'INSERT INTO "Contents"' + \
+            f'(id, "createdAt", "updatedAt", url, html, {", ".join(attrs)}) ' + \
+            'VALUES (DEFAULT, ' + \
+            ", ".join("${}".format(i) for i in range(1, 5 + len(attrs))) + \
+            ') RETURNING id;'
         print(q)
-        await conn.execute(q, url, html, *[article[a] for a in article_attrs])
-        print('inserted')
+        now = datetime.datetime.now()
+        r = await conn.fetchval(q, now, now, url, html,
+                                *[getattr(article, a) or None for a in attrs])
+        print('Inserted in database id =', r)
+        return r
 
     # Subscribe to a subject. Create queues.
     # https://docs.nats.io/developing-with-nats/receiving/queues
+    print('Connecting to NATS')
     urls_queue = await nc.subscribe('urls', queue='scrapers', cb=urls_handler)
     queues = [urls_queue]
 
@@ -86,12 +101,8 @@ async def run(loop):
     for sig in ('SIGINT', 'SIGTERM'):
         loop.add_signal_handler(getattr(signal, sig), signal_handler)
 
-    u = await conn.fetch("SELECT * FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema = 'public'")
+    u = await conn.fetch('SELECT * FROM "Users";')
     print(u)
-
-    u = await conn.fetch('SELECT * FROM "Users"')
-    print(u)
-
 
 
 if __name__ == '__main__':
